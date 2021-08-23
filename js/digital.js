253f8fb89f015ce877a34f7aa33a729c7308c395
turtle601 <turtle601@naver.com>
2021-08-23 13:05:02 +0900

    [Feat] Make digital Clock Page

---
+++ js/digital.js
@@ -0,0 +1,26 @@
+let digitalClock = document.getElementById("now");
+
+function clock(){
+    const weekList = ["일", "월", "화", "수", "목", "금", "토"];
+
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const week = date.getDay();
+    const day = date.getDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    second = second < 10 ? "0" + second : second;
+
+    digitalClock.innerText = `${year}년 ${month + 1}월 ${day}일 ${weekList[week]} 요일 ${hour} : ${minute} : ${second}`;
+
+}
+
+function init(){
+    clock();
+    setInterval(clock,1000); // 1초마다 clock 함수 반복 실행
+}
+
+init();
